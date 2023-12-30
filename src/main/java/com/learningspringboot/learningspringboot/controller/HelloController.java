e22a264c33078c2147c94853d9d50ae395cf6326
mdimran1409036 <mdimran1409036@gmail.com>
2023-12-30 14:37:30 +0600

    custom configuration(welcome message) added and applicaiton.yml also added

--- src/main/java/com/learningspringboot/learningspringboot/controller/HelloController.java
+++ src/main/java/com/learningspringboot/learningspringboot/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.learningspringboot.learningspringboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String message;
     @GetMapping(path = "/hello")
     public String sayHello(){
-        return "hello world!!!!";
+        return this.message;
     }
     @GetMapping(path="/random-number")
     public Integer getRandomNumber(){
